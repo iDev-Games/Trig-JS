b1d7f60a596f15ab40216292724f677554e78632
dev <admin@idevgames.co.uk>
2023-03-29 20:10:06 +0000

    v1.7.1 - Refactoring of script.

--- src/trig.js
+++ src/trig.js
@@ -1,71 +1,84 @@
-/* Trig.js v1.7.0 by iDev Games */
-document.addEventListener('DOMContentLoaded', initTrig, false);
+/* Trig.js v1.7.1 by iDev Games */
 
-function initTrig() {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(function(entry) {
-            const intersecting = entry.isIntersecting;
-            if (intersecting) {
-                entry.target.classList.add("trig");
-            } else {
-                entry.target.classList.remove("trig");
-            }
-            var offset = 0;
-            var min = -100;
-            var max = 100;
-            var el = entry.boundingClientRect.top;
-            if (entry.target.dataset.trigOffset) {
-                offset = parseInt(entry.target.dataset.trigOffset);
-            }
-            if (entry.target.dataset.trigMin) {
-                min = parseInt(entry.target.dataset.trigMin);
-            }
-            if (entry.target.dataset.trigMax) {
-                max = parseInt(entry.target.dataset.trigMax);
-            }
-            var posTop = 0 - (el - ((documentHeight / 2) + offset));
-            var pos = (posTop / documentHeight) * 100;
-            if (pos >= min && pos <= max) {
-                thePos[entry.target.index] = pos;
-            } else if (pos <= min) {
-                thePos[entry.target.index] = min;
-            } else if (pos >= max) {
-                thePos[entry.target.index] = max;
-            }
-        });
-        updatePos();
-        observer.disconnect();
-    });
-    var trigs = document.querySelectorAll('.enable-trig,[data-trig]');
-    var thePos = [];
-    var documentHeight = 0;
-    document.addEventListener('scroll', trigScroll, false);
-    document.addEventListener('resize', trigInit, false);
-    trigInit();
+const observer = new IntersectionObserver(function(entries) {
+    trigObject.trigEntries(entries);
+    trigObject.updatePos(trigs);
+    observer.disconnect();
+});
 
-    function trigInit(){
-        documentHeight = innerHeight;
-        trigScroll();
-    }
-
-    function trigScroll() {
-        if (trigs) {
-            trigs.forEach(function(element, index) {
-                element.index = index;
-                observer.observe(element);
-            });
+const trig = {
+    trigs: [],
+    thePos: [],
+    documentHeight: 0,
+    trigIntersecting: function(entry) {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("trig");
+        } else {
+            entry.target.classList.remove("trig");
         }
-    }
-
-    function updatePos() {
+    },
+    trigEntries: function(entries) {
+        entries.forEach(function(entry) {
+            trigObject.trigIntersecting(entry);
+            trigObject.trigPos(entry);
+        });
+    },
+    trigSetPos: function(pos, min, max, entry) {
+        if (pos >= min && pos <= max) {
+            trigObject.thePos[entry.target.index] = pos;
+        } else if (pos <= min) {
+            trigObject.thePos[entry.target.index] = min;
+        } else if (pos >= max) {
+            trigObject.thePos[entry.target.index] = max;
+        }    
+    },
+    trigPos: function(entry) {
+        var offset = 0;
+        var min = -100;
+        var max = 100;
+        var el = entry.boundingClientRect.top;
+        if (entry.target.dataset.trigOffset) {
+            offset = parseInt(entry.target.dataset.trigOffset);
+        }
+        if (entry.target.dataset.trigMin) {
+            min = parseInt(entry.target.dataset.trigMin);
+        }
+        if (entry.target.dataset.trigMax) {
+            max = parseInt(entry.target.dataset.trigMax);
+        }
+        var posTop = 0 - (el - ((trigObject.documentHeight / 2) + offset));
+        var pos = (posTop / trigObject.documentHeight) * 100;
+        trigObject.trigSetPos(pos, min, max, entry);
+    },
+    updatePos: function(trigs) {
         trigs.forEach(function(element, index) {
             var el = element.style;
-            el.setProperty('--trig', thePos[index] + "%");
-            el.setProperty('--trig-reverse', -(thePos[index]) + "%");
-            el.setProperty('--trig-px', thePos[index] + "px");
-            el.setProperty('--trig-px-reverse', -(thePos[index]) + "px");
-            el.setProperty('--trig-deg', ((thePos[index] / 100) * 360) + "deg");
-            el.setProperty('--trig-deg-reverse', ((-(thePos[index]) / 100) * 360) + "deg");
+            el.setProperty('--trig', trigObject.thePos[index] + "%");
+            el.setProperty('--trig-reverse', -(trigObject.thePos[index]) + "%");
+            el.setProperty('--trig-px', trigObject.thePos[index] + "px");
+            el.setProperty('--trig-px-reverse', -(trigObject.thePos[index]) + "px");
+            el.setProperty('--trig-deg', ((trigObject.thePos[index] / 100) * 360) + "deg");
+            el.setProperty('--trig-deg-reverse', ((-(trigObject.thePos[index]) / 100) * 360) + "deg");
+        });
+    }
+};
+  
+const trigObject = Object.create(trig);
+document.addEventListener('scroll', trigScroll, false);
+document.addEventListener('resize', trigInit, false);
+document.addEventListener('DOMContentLoaded', trigInit, false);
+
+function trigInit(){
+    trigs = document.querySelectorAll('.enable-trig,[data-trig]');
+    trigObject.documentHeight = innerHeight;
+    trigScroll();
+}
+
+function trigScroll() {
+    if (trigs) {
+        trigs.forEach(function(element, index) {
+            element.index = index;
+            observer.observe(element);
         });
     }
 }
